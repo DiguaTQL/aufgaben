95a6b54e9b3af30222fae157fa57ecd66afe2528
Digua <tengweilong@outlook.com>
2021-05-08 09:14:23 +0200

    #stern_dreieck

--- aufgabe_let/stern_dreieck_umrechnen.c
+++ aufgabe_let/stern_dreieck_umrechnen.c
@@ -1,30 +1,57 @@
 #include <stdio.h>
 #include <math.h>
 #include <complex.h>
+#include <math.h>
 const double PI=3.14159265;
 
 int main()
 {
-    double r1s, r1d ,r2s, r2d, r3s, r3d;
+    double r3s, r5s, U12, T;
+    complex double r3d,r4d,r5d,r4s, Z1, Z2, U1, U2, I1, I2, I5, Z1_R3d, Z2_R5d, Rg;
 
     /*
-    printf("R1 in Stern:");
-    scanf("%lf",&r1s);
-
-    printf("R2 in Stern:");
-    scanf("%lf",&r2s);
+    ---------Z1-----------Z2--------
+    |               |               |
+    |               |               |  
+    |               R5s             |
+    |               |               |
+    |               |               |  
+    ---------R3s-----------Z4s--------
     
-    printf("R3 in Stern:");
-    scanf("%lf",&r3s);
     */
-    r1s=12.8;
-    r2s=8.6;
-    r3s=4.8*cexp(-I*45.0/180.0*PI);
+    Z1=4.8*cexp(I*30./180.*PI);
+    Z2=6.2-I*2.3;
+    
+    /*Ra*/r3s=12.8;
+    /*Rb*/r5s=8.6;
+    /*Rc*/r4s=4.8*cexp(-I*45.0/180.0*PI);
+ 
+    r3d=((r3s*r5s)/r4s)+r3s+r5s;
+    r5d=((r4s*r5s)/r3s)+r4s+r5s;
+    r4d=((r3s*r4s)/r5s)+r3s+r4s;
 
-    r1d=r1s*r3s/(r1s+r2s+r3s);
-    r2d=r1s*r2s/(r1s+r2s+r3s);
-    r3d=r2s*r3s/(r1s+r2s+r3s);
+    printf("\nR3d in Dreieck: %g \u03A9\nR5d in Dreieck: %g \u03A9\nR4d in Dreieck: %g \u03A9\n",r3d,r5d,r4d);
 
-    printf("\nR1 in Dreieck: %g \u03A9\nR3 in Dreieck: %g \u03A9\nR3 in Dreieck: %g \u03A9\n",r1d,r2d,r3d);
+    /*
+    ---------Z1-----------Z2--------|
+    |               |               |
+    |               |               |  
+    |               |               |
+    |               |               |
+    |               |               |  
+    ---------R3d-----------R5d------|
+    |                               |
+    |---------------R4d-------------|
+    |                               |
+    1                               2 
+    */
+    U12=10.*sin(2.*PI*50.*0);//T muss noch inplemtieren
+    Z1_R3d=1./(1./Z1+1./r3d);
+    Z2_R5d=1./(1./Z2+1./r5d);
+    Rg=1./(1./r4d+1./(Z1_R3d+Z2_R5d));
+
+    printf("\nRg ist :%g \u03A9\n",Rg);
+
+
 
 }
