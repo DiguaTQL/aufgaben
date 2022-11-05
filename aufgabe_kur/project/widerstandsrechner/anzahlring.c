eeced7685b82447ae2f272abbffb6a3c9c61c019
Digua <tengweilong@outlook.com>
2022-11-05 10:29:19 +0100

    ITA

---
+++ aufgabe_kur/project/widerstandsrechner/anzahlring.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int AnzahlRing(){
+    int Anzahl;
+    printf("Bitte geben Sie die Anzahl der Ringe ein:");
+    scanf("const char *restrict, ...")
+
+    return Anzahl;
+}
--- aufgabe_kur/project/widerstandsrechner/main.c
+++ aufgabe_kur/project/widerstandsrechner/main.c
@@ -1,2 +1,29 @@
 #include <stdio.h>
 
+int main(void){
+    /*
+    1.Abfragen Anzahl der Ringe             von anzahlring.c Rückgabe int.
+    2.Eingabe/Fabecode Beispeile fr-fr-fr-fr-fr so...       von 
+    3.if Eingabe nicht korrekt, dann analysieren welche Feherl und Fehler Tipps ausgeben
+    3.5 Eingabe wieder abfragen
+    4.wenn Eingabe richtig sind, dann berechnen.
+    5.Wert anzeigen
+
+    Struct resistor{
+        int Ring_1_nr, Ring_2_nr, Ring_3_nr, Multifakt, Tempko;
+        char Toleranz[10];
+    }
+
+
+    */
+    // 1
+    int iRingAnzahl;
+    
+    printf("Bitte geben Sie Anzahl der Ring ein:");
+    scanf("&i",&iRingAnzahl);
+
+
+
+
+
+}
