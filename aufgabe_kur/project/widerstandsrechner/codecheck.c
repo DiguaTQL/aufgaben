b52efd075bdc8805f7282b66b70e4157c06197be
Digua <tengweilong@outlook.com>
2022-11-17 19:59:18 +0100

    ITA

--- aufgabe_kur/project/widerstandsrechner/codecheck.c
+++ aufgabe_kur/project/widerstandsrechner/codecheck.c
@@ -3,5 +3,13 @@
 
 int codecheck(char *farbecode, int AnzahlRing, int languageNr){
     puts(farbecode);
+    char *w1;
+    int lauf=0;
+    while (farbecode[lauf]!='-') {
+        w1[lauf]=farbecode[lauf];
+        ++lauf;
+    }
+    //printf("%s",w1);
+    //puts(w1);
     return 0;
 }
