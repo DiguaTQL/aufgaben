243e67daafe6d0b4da6b675aaff6e579ff1bd44f
Digua <tengweilong@outlook.com>
2022-11-24 20:33:20 +0100

    ITA

---
+++ aufgabe_kur/project/widerstandsrechner/auswerten.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "resistor.h"
+#include "findminus.c"
+#include "split.c"
+
+struct resistor auswerten( char *farbecode, int AnzahlRing){
+    struct resistor x;
+    int minus=findeminus(farbecode);
+    
+    return x;
+}
