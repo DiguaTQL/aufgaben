bba9173f5d403c77ca7d4a46835bc790a1c8bd81
Digua <tengweilong@outlook.com>
2023-02-04 02:10:22 +0100

    ITA

--- aufgabe_kur/3_jahr/c64a1.c
+++ aufgabe_kur/3_jahr/c64a1.c
@@ -1,10 +1,9 @@
-#include <cstddef>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(void){
-    double *mw=NULL;
+    char *mw=NULL;
     int idx_1, idx_2;
     char text[]="Hello,World!\n";
     idx_1=strlen(text)+1;
@@ -13,11 +12,15 @@ int main(void){
     //scanf("%i",&idx_1);    
     //while (getchar()!='\n') {}
 
-    mw=malloc(idx_1*sizeof(double));
+    mw=malloc(idx_1*sizeof(char));
     if (mw==NULL) {
         printf("malloc-Error!\n");
         return 0;
     }
+    strcpy(mw, text);
+    printf("%s", mw);
 
+    free(mw);
+    return 0;
 
 }
