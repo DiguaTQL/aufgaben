6fba4b9049ded3fda05ac8f44ccb14eac20b4227
Digua <tengweilong@outlook.com>
2023-05-19 04:23:49 +0200

    ITA

--- aufgabe_kur/project/ausgleichsgerade/grafik.c
+++ aufgabe_kur/project/ausgleichsgerade/grafik.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <liner.h>
 #include <unistd.h>
+#include <math.h>
 
 void grafik(double** liste, int Anzahl){
     /*
@@ -52,6 +53,28 @@ void grafik(double** liste, int Anzahl){
     m = (Anzahl * sigma_xy - sigma_x * sigma_y)/(Anzahl * sigma_x2 - sigma_x * sigma_x);    // m berechnen
 
 
+    /*
+        SVG
+            --> x+
+         |  xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+         |  x                                 x
+         v  x |                               x
+            x |                x              x
+         y  x |                               x
+         +  x |                               x
+            x |                               x
+            x |             x                 x
+            x |                               x
+            x |    x                          x
+            x |                               x
+            x |                               x
+            x -----------------------------   x
+            xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        
+    */
+
+    double svg_width =1000, svg_height =1000, x_offset=50,y_offset=50, x_maxscale=10,y_maxscale=10;
+
     FILE *dm;
     char dname[256] = "ausgleichsgerade.svg";
     dm = fopen(dname,"w");
@@ -61,62 +84,73 @@ void grafik(double** liste, int Anzahl){
     }
 
     fprintf(dm, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-    fprintf(dm, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"550\" height=\"500\">\n");
+    fprintf(dm, "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%g\" height=\"%g\">\n",svg_width,svg_height);
 
     // Bestimmung der Größe des Koordinatensystems(Max-Wert)
     double xMax = liste[0][0];
     double yMax = liste[0][1];
     double xMin = liste[0][0];
     double yMin = liste[0][1];
-    for (int i = 0; i < Anzahl; ++i) {
-        if (liste[i][0] > xMax) {xMax = liste[i][0];}
-        if (liste[i][1] > yMax) {yMax = liste[i][1];}
-        if (liste[i][0] < xMin) {xMin = liste[i][0];}
-        if (liste[i][1] < yMin) {yMin = liste[i][1];}}
-    
-    double xStep = xMax / 10.0;
-    double yStep = yMax / 10.0;
 
+    for (int i = 0; i < Anzahl; ++i) {
+        if (liste[i][0] > xMax)
+            xMax = liste[i][0];
+        if (liste[i][1] > yMax)
+            yMax = liste[i][1];
+        if (liste[i][0] < xMin)
+            xMin = liste[i][0];
+        if (liste[i][1] < yMin) 
+            yMin = liste[i][1];
+    }
+    
+    double xStep = ceil(xMax / x_maxscale);
+    double yStep = ceil(yMax / y_maxscale);
+    
     // Berechnung der Pixelabstände zwischen den Koordinatenachsen
-    double xScale = 400/xMax;
-    double yScale = 400/yMax;
- 
+    double xScale = (svg_width - x_offset * 2) / (xStep * x_maxscale);
+    double yScale = (svg_height - y_offset * 2) / (yStep * y_maxscale);
     // Zeichnen der x-Achse
-    fprintf(dm, "<line x1=\"50\" y1=\"450\" x2=\"450\" y2=\"450\" stroke=\"black\" stroke-width=\"2\" />\n");	// x-Achse
+    fprintf(dm, "<line x1=\"%g\" y1=\"%g\" x2=\"%g\" y2=\"%g\" stroke=\"black\" stroke-width=\"2\" />\n",x_offset, svg_height - y_offset, svg_width-x_offset, svg_height - y_offset);	// x-Achse
    
-    for (double i = xMin; i < xMax; i += xStep) {
-        int xPos = 50 + (i - xMin) * xScale;
-    	fprintf(dm, "<line x1=\"%d\" y1=\"450\" x2=\"%d\" y2=\"460\" stroke=\"black\" />\n", xPos, xPos);
-    	fprintf(dm, "<text x=\"%d\" y=\"480\" font-size=\"12\">%.1f</text>\n", xPos, i-liste[0][0]);
+    for (double i = 0; i <= x_maxscale*xStep ; i += xStep) {
+        int xPos = x_offset + i * xScale;
+    	fprintf(dm, "<line x1=\"%d\" y1=\"%g\" x2=\"%d\" y2=\"%g\" stroke=\"black\" />\n", xPos, svg_height-y_offset, xPos, svg_height-y_offset+10);
+    	fprintf(dm, "<text x=\"%d\" y=\"%g\" font-size=\"12\">%.1f</text>\n", xPos, svg_height-y_offset+30, i);
     }
 
     // Zeichnen der y-Achse
-    fprintf(dm, "<line x1=\"50\" y1=\"50\" x2=\"50\" y2=\"450\" stroke=\"black\" stroke-width=\"2\" />\n");	// y-Achse
+    fprintf(dm, "<line x1=\"%g\" y1=\"%g\" x2=\"%g\" y2=\"%g\" stroke=\"black\" stroke-width=\"2\" />\n",x_offset, svg_height - y_offset, x_offset, y_offset);	// y-Achse
     
-    for (double i = yMin; i <= yMax; i += yStep) {
-	    int yPos = 450 - (i - yMin) * yScale;
-	    fprintf(dm, "<line x1=\"40\" y1=\"%d\" x2=\"50\" y2=\"%d\" stroke=\"black\" />\n", yPos, yPos);
-	    fprintf(dm, "<text x=\"10\" y=\"%d\" font-size=\"12\">%.1f</text>\n", yPos + 5, i-liste[0][1]);
+    for (double i = 0; i <= y_maxscale*yStep ; i += yStep) {
+	    int yPos = svg_height-y_offset - i  * yScale;
+	    fprintf(dm, "<line x1=\"%g\" y1=\"%d\" x2=\"%g\" y2=\"%d\" stroke=\"black\" />\n",x_offset-10, yPos, y_offset, yPos);
+	    fprintf(dm, "<text x=\"10\" y=\"%d\" font-size=\"12\">%.1f</text>\n", yPos + 5, i);
     }
 
     // Beschriftung der Achsen
-    fprintf(dm, "<text x=\"480\" y=\"480\" text-anchor=\"middle\">X-Achse</text>\n");
-    fprintf(dm, "<text x=\"200\" y=\"250\" transform=\"rotate(-90 20,250)\" text-anchor=\"middle\">Y-Achse</text>\n");
+    fprintf(dm, "<text x=\"%g\" y=\"%g\" text-anchor=\"middle\">X-Achse</text>\n",svg_width-90,svg_height-5);
+    fprintf(dm, "<text x=\"200\" y=\"275\" transform=\"rotate(-90 20,275)\" text-anchor=\"middle\">Y-Achse</text>\n");
 
     // Zeichnen der Datenpunkte
     for (int i = 0; i < Anzahl; i++) {
-        int x_pos = 50 + liste[i][0] * xScale;
-        int y_pos = 450 - liste[i][1] * yScale;
+        int x_pos = x_offset + liste[i][0] * xScale;
+        int y_pos = svg_height-y_offset - liste[i][1] * yScale;
         fprintf(dm, "<circle cx=\"%d\" cy=\"%d\" r=\"3\" fill=\"blue\" />\n", x_pos, y_pos);
         fprintf(dm, "<text x=\"%d\" y=\"%d\" font-size=\"8\">P%d (%.1f, %.1f)</text>\n", x_pos+10, y_pos, i+1, liste[i][0], liste[i][1]);
     }
 
-    /// Zeichnen der Regressionsgerade
+    // Zeichnen der Regressionsgerade
+    // y = mx +b
     double y_start = b * yScale;
-    double y_end = yMax * yScale;
-    double x_end = xMax * xScale;
+    double y_end = (xStep*x_maxscale*m+b) * yScale;
+    double x_end = (yStep*y_maxscale-b)/m *xScale;
     
-    fprintf(dm, "<line x1=\"50\" y1=\"%f\" x2=\"%f\" y2=\"%f\" stroke=\"red\" stroke-width=\"2\" />\n", 450 - y_start, 50+x_end, 450 - y_end);
+    if (x_end+x_offset < svg_width-x_offset*2) 
+        fprintf(dm, "<line x1=\"%g\" y1=\"%f\" x2=\"%f\" y2=\"%f\" stroke=\"red\" stroke-width=\"2\" />\n",x_offset, svg_height-y_offset - y_start, x_offset + x_end, y_offset);
+    else{
+        x_end = xStep*10 *xScale;
+        fprintf(dm, "<line x1=\"%g\" y1=\"%f\" x2=\"%f\" y2=\"%f\" stroke=\"red\" stroke-width=\"2\" />\n",x_offset, svg_height-y_offset - y_start, x_offset + x_end, svg_height-y_offset - y_end);
+    }
 
     fprintf(dm, "</svg>\n");
     fclose(dm);
@@ -128,7 +162,5 @@ void grafik(double** liste, int Anzahl){
     sprintf(command, "inkscape ausgleichsgerade.svg");
     system(command);
     return ;
-
-
 
 }
