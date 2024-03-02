3daf54090ef7dbf61ebe60ea028047369143e45f
Digua <tengweilong@outlook.com>
2024-03-02 08:58:26 +0100

    ITA

--- aufgabe_kur/4_jahr/c610L.c
+++ aufgabe_kur/4_jahr/c610L.c
@@ -2,7 +2,7 @@
 #include <time.h>
 
 int main(void){
-    struct tm gt={.tm_hour=8,.tm_year=100,.tm_mon=9,.tm_mday=13}; //13.10.2000 //tm fämgt jahr 1900
+    struct tm gt={.tm_hour=8,.tm_year=100,.tm_mon=9,.tm_mday=13}; //13.10.2000 8:00 //tm fämgt jahr 1900
     time_t getsec, jetzt;
     jetzt=time(NULL);
     getsec = mktime(&gt);
